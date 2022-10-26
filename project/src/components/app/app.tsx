4f13cb7a566a8470f390bea431b528726d497cd6
Olga Surovtseva <olga.surovtseva.94@mail.ru>
2022-10-27 01:46:49 +0500

    создала компонент для страницы 404. настроила маршруты для отрисовки страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import HomePage from '../../pages/home-page/home-page';
 import PlaceCard from '../../components/place-card/place-card';
+// import HomePageEmpty from '../../pages/home-page-empty/home-page-empty';
+import LoginPage from '../../pages/login-page/login-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+// import PropertyNotLoggedPage from '../../pages/property-not-logged-page/property-not-logged-page';
+import PropertyPage from '../../pages/property-page/property-page';
+
 
 type AppPageProps = {
   placeCount: number;
@@ -8,9 +16,30 @@ type AppPageProps = {
 
 function App({placeCount, amountCount}: AppPageProps): JSX.Element {
   return (
-    <HomePage placesCount={placeCount}>
-      <PlaceCard amountCount={amountCount}/>
-    </HomePage>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={
+            <HomePage placesCount={placeCount}>
+              <PlaceCard amountCount={amountCount}/>
+            </HomePage>
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<PropertyPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
