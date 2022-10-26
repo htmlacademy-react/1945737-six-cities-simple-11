902da75319dfc715a01829a50b2cb829c79c6984
Olga Surovtseva <olga.surovtseva.94@mail.ru>
2022-10-27 02:09:42 +0500

    заменила код разметки в компонентах pages на вызов компонента Logo

--- project/src/pages/home-page-empty/home-page-empty.tsx
+++ project/src/pages/home-page-empty/home-page-empty.tsx
@@ -1,3 +1,4 @@
+import Logo from '../../components/logo/logo';
 
 function HomePageEmpty(): JSX.Element {
   return (
@@ -9,11 +10,7 @@ function HomePageEmpty(): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active" href="/#">
-                <img className="header__logo" src="img/logo.svg" alt="six cities logo" width="81" height="41"/>
-              </a>
-            </div>
+            <Logo/>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
--- project/src/pages/not-found-page/not-found-page.tsx
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function NotFoundPage(): JSX.Element {
   return (
@@ -10,11 +11,7 @@ function NotFoundPage(): JSX.Element {
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
+            <Logo/>
           </div>
         </div>
       </header>
